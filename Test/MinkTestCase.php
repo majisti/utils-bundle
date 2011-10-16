6568e06a9fbcb343299db97516ae9d503d5c14c1
Steven Rosato <steven.rosato@majisti.com>
2011-10-16 14:06:55 -0400

    Fixed class name anomalies.

--- Test/MinkTestCase.php
+++ Test/MinkTestCase.php
@@ -2,7 +2,7 @@
 
 namespace Majisti\UtilsBundle\Test;
 
-use Behat\MinkBundle\Test\MinkTestCase;
+use Behat\MinkBundle\Test\MinkTestCase as BaseMinkTestCase;
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\Loader;
 
@@ -11,13 +11,13 @@ use Doctrine\Common\DataFixtures\Loader;
  * 
  * @author Steven Rosato
  */
-class AbstractTestCase extends MinkTestCase implements PersistentTest
+class MinkTestCase extends BaseMinkTestCase implements PersistentTest
 {
     /*
      * (non-phpDoc) 
      * @see Inherited documentation.
      */
-    public function loadFixtures($classnames = array())
+    public function loadFixtures(array $classnames)
     {
         $fp = new FixturesProxy($this->createKernel());
         $fp->load($classnames);
